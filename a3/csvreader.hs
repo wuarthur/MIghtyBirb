4959b60a3a38a2aaced7df0b5b625ad83b351ac0
Carol Leung <carol.leung@xerusmedical.com>
2018-09-29 17:46:15 -0700

    ugly bug fix but it works so eh

--- a3/csvreader.hs
+++ a3/csvreader.hs
@@ -1,22 +1,25 @@
 splitsep :: (a -> Bool) -> [a] -> [[a]]
 
 splitsep _ [] = [[]]
-splitsep sep lst = (beforesep sep lst): splitsep sep (aftersep sep lst)
+splitsep sep lst = if length (beforesep sep lst) == length lst then [lst] else (beforesep sep lst):splitsep sep (aftersep sep lst)
   where
-      --beforesep sep lst gets all of the chars before the first instance of sep
-      beforesep _ [] = []
-      beforesep sep (h:t)
-        | sep h = []
-        | otherwise = h:beforesep sep t
-      --aftersep sep lst gets all of the chars after the first instance of sep
-      aftersep _ [] = []
-      aftersep sep (h:t)
-        | sep h = t
-        | otherwise = aftersep sep t
+    --beforesep sep lst gets all of the chars before the first instance of sep
+    beforesep _ [] = []
+    beforesep sep (h:t)
+      | sep h = []
+      | otherwise = h:beforesep sep t
+    --aftersep sep lst excludes all of the chars after the first instance of sep
+    aftersep _ [] = []
+    aftersep sep (h:t)
+       | sep h = t
+       | otherwise = aftersep sep t
 
 {-
+splitsep (==',') "comma,separated,list,as,in,a,csv,file" => ["comma","separated","list","as","in","a","csv","file"]
+splitsep (==',') "csv,,with,missing,elts,,," => ["csv","","with","missing","elts","","",""]
+splitsep (`elem` " ,.?!") "What? is this thing? ... called Love." => ["What","","is","this","thing","","","","","","called","Love",""]
+splitsep (==',') [] => [""]
 splitsep (\ x -> mod x 2 == 0) [1,3,4,5,7,9,8,8,55,45,48] => [[1,3],[5,7,9],[],[55,45],[]]
-splitsep (==',') "comma,separated,list,as,in,a,csv,file" => ["comma","separated","list","as","in","a","csv","file",""]
 -}
 
 csvreader filename =
@@ -25,5 +28,5 @@ csvreader filename =
     print [ splitsep (==',') x | x <- (splitsep (=='\n') file)]
 
 {-
-csvreader "test.csv" => [["Day","Month","Received","Sold",""],["12","May","200","20",""],["10","July","","23",""],[""]]
+csvreader "test.csv" => [["Day","Month","Received","Sold"],["12","May","200","20"],["10","July","","23"],[""]]
 -}
