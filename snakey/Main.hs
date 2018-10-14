b4652176d29d8ba1d0d7a5c00dab59c2c9d57757
Carol Leung <carol.leung@xerusmedical.com>
2018-10-13 20:01:36 -0700

    made snake game (did up to the animations part of the tutorial)

--- snakey/Main.hs
+++ snakey/Main.hs
@@ -1,14 +1,16 @@
-module Main(main) where
+module Main(main, SnakeGame, render, initialState) where
 
 import Graphics.Gloss
 
+grid, speed :: Float
+grid = 20
+speed = 1  -- so we move 1 grid space per speed seconds
+
 width, height, offset :: Int
-width = 300
-height = 300
+width = 20 * 50 --change the 20 to grid TODO
+height = 20 * 50
 offset = 100
 
-grid :: Float
-grid = 20
 
 window :: Display
 window = InWindow "Snakey" (width, height) (offset, offset)
@@ -16,21 +18,36 @@ window = InWindow "Snakey" (width, height) (offset, offset)
 background :: Color
 background = white
 
-food :: Picture
-food = translate startX startY $ color foodColor $ circleSolid (grid / 2)
-  where
-    foodColor = red
-    startX = -20
-    startY = -100
+-- Ddescribe the state of the game.
+data SnakeGame = Game
+  { foodLoc :: (Float, Float)
+  , snakeLoc :: (Float, Float)
+  , snakeDir :: Char -- N S E W
+  } deriving Show
 
-snake :: Picture
-snake = rectangleSolid grid grid
+
+-- | The starting state for the game of Pong.
+initialState :: SnakeGame
+initialState = Game
+  { foodLoc = (-grid, grid*5)
+  , snakeLoc = (0, 0)         -- center of the screen
+  , snakeDir = 'N'
+  }
+
+-- | Convert a game state into a picture.
+render :: SnakeGame  -- ^ The game state to render.
+       -> Picture   -- ^ A picture of this game state.
+render game =
+  pictures [food, snake]
+  where
+    --  The pong ball.
+    food = uncurry translate (foodLoc game) $ color foodColor $ circleSolid 10
+    foodColor = red
+
+    snake = uncurry translate (snakeLoc game) $ rectangleSolid grid grid
 
 drawing :: Picture
-drawing = pictures
-  [ food
-  , snake
-  ]
+drawing = render initialState
 
 main :: IO ()
 main = display window background drawing
