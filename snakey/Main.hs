4cf8aa0892bac3d41b53fd2122be6c34dde64d04
yi <dwu@wuarthurs-MacBook-Air.local>
2018-10-14 16:42:38 -0700

    broke broke

--- snakey/Main.hs
+++ snakey/Main.hs
@@ -7,6 +7,7 @@ width = 300
 height = 300
 offset = 100
 
+
 grid :: Float
 grid = 20
 
@@ -26,11 +27,32 @@ food = translate startX startY $ color foodColor $ circleSolid (grid / 2)
 snake :: Picture
 snake = rectangleSolid grid grid
 
+
+realSnake = [(0,0), (20,0), (40,0), (60,0)]
+
+updateSnake :: Float -> Float -> [(Float, Float)] -> [(Float, Float)]
+updateSnake _ _ [] = []
+updateSnake x y (h:t)=
+    do
+        newX<-fst h
+        newY<-snd h
+        return ((x,y):(updateSnake newX newY t))
+
+moveSnake dir (h:t)
+    | dir == "UP" =  ((fst h)+grid, (snd h)+grid):(updateSnake (fst h) (snd h) t)
+
+newslake = moveSnake "UP" realSnake
+
+drawSnake [] = []
+drawSnake (h:t) = (drawPart h):(drawSnake t)
+
+drawPart (x,y) = translate x y (rectangleSolid grid grid)
+
 drawing :: Picture
 drawing = pictures
-  [ food
-  , snake
-  ]
+  (drawSnake newslake)
 
 main :: IO ()
 main = display window background drawing
+
+
