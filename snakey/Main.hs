c277191e31fb38ecd2779637e3bb72e6dc0abe64
Carol Leung <carol.leung@xerusmedical.com>
2018-10-15 12:51:14 -0700

    refactoring. starting on food

--- snakey/Main.hs
+++ snakey/Main.hs
@@ -45,14 +45,20 @@ render game =
     food = uncurry translate (foodLoc game) $ color foodColor $ circleSolid 10
     foodColor = red
     snake = drawSnake (snakeLoc game)
+    drawSnakeH  [] = []
+    drawSnakeH  (h:t) = (drawPart h):(drawSnakeH t)
+    --actually draws the snake
+    drawSnake list = (pictures (drawSnakeH list))
+    drawPart (x,y) = translate x y (rectangleSolid grid grid)
 
+
+--food
+genFood :: SnakeGame -> (Float, Float)
+genFood game
+  | head (snakeLoc game) == (foodLoc game) = (foodLoc game) --randomly generate food
+  | otherwise = (foodLoc game)
 
 --snake render and move
---updateSnake :: Float -> Float -> [(Float, Float)] -> [(Float, Float)]
-updateSnake _ _ [] = []
-updateSnake x y (h:t)= (x,y):(updateSnake (fst h) (snd h) t)
-
---keep the state somewhere of the snake to make sure it cannot move backward
 moveSnake dir (h:t)
     | dir == 'w' =  (x, y+grid):(updateSnake x y t)
     | dir == 's' =  (x, y-grid):(updateSnake x y t)
@@ -61,48 +67,18 @@ moveSnake dir (h:t)
     where
       x = fst h
       y = snd h
-
-drawSnakeH  [] = []
-drawSnakeH  (h:t) = (drawPart h):(drawSnakeH t)
---actually draws the snake
-drawSnake list = (pictures (drawSnakeH list))
-drawPart (x,y) = translate x y (rectangleSolid grid grid)
---snake render and move
+      --updateSnake :: Float -> Float -> [(Float, Float)] -> [(Float, Float)]
+      updateSnake _ _ [] = []
+      updateSnake x y (h:t)= (x,y):(updateSnake (fst h) (snd h) t)
 
 
 -- | Respond to key events.
--- For an 's' keypress, reset the ball to the center.
-handleKeys (EventKey (Char 'w') _ _ _) game
-   |(snakeDir game) == 's' = game
-   |otherwise =
-    game { foodLoc = (foodLoc game)
-   , snakeLoc = (moveSnake 'w' (snakeLoc game))        -- center of the screen
-   , snakeDir = 'w'
-   }
-
-handleKeys (EventKey (Char 's') _ _ _) game
-   |(snakeDir game) == 'w' = game
-   |otherwise =
-   game { foodLoc = (foodLoc game)
-   , snakeLoc = (moveSnake 's' (snakeLoc game))        -- center of the screen
-   , snakeDir = 's'
-   }
-
-handleKeys (EventKey (Char 'd') _ _ _) game
-    |(snakeDir game) == 'a' = game
-    |otherwise =
-    game { foodLoc = (foodLoc game)
-    , snakeLoc = (moveSnake 'd' (snakeLoc game))        -- center of the screen
-    , snakeDir = 'd'
-}
-
-handleKeys (EventKey (Char 'a') _ _ _) game
-   |(snakeDir game) == 'd' = game
-   |otherwise =
-    game { foodLoc = (foodLoc game)
-    , snakeLoc = (moveSnake 'a' (snakeLoc game))        -- center of the screen
-    , snakeDir = 'a'
- }
+handleKeys (EventKey (Char key) _ _ _) game
+  | key == 'w' && (snakeDir game) /= 's' = game { snakeDir = key }
+  | key == 's' && (snakeDir game) /= 'w' = game { snakeDir = key }
+  | key == 'a' && (snakeDir game) /= 'd' = game { snakeDir = key }
+  | key == 'd' && (snakeDir game) /= 'a' = game { snakeDir = key }
+  | otherwise = game
 -- Do nothing for all other events.
 handleKeys _ game = game
 
@@ -113,6 +89,5 @@ update second game =
         , snakeDir = (snakeDir game)
      }
 
---(pictures (drawSnake realSnake))
 main :: IO ()
 main = play window background speed initialState render handleKeys update
