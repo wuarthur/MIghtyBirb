c571b8ec6e466b0b5259d860ec9c2e6899406568
yi <dwu@wuarthurs-MacBook-Air.local>
2018-11-15 18:16:42 -0800

    starter code for prolog project

---
+++ Mr_Prolog/tes.pl
@@ -0,0 +1,23 @@
+
+:-use_module(library(http/http_open)).
+
+:- use_module(library(http/thread_httpd)).
+:- use_module(library(http/http_dispatch)).
+
+
+
+go:-
+    use_module(library(http/http_open)),
+   http_open('http://www.google.com/search?q=prolog', In, []),
+   copy_stream_data(In, user_output),
+   close(In).
+
+
+
+server(Port) :-
+        http_server(http_dispatch,
+                    [ port(Port)
+                    ]).
+
+:- http_handler(root(.), entry_page, []).
+:- http_handler(root(home), home_page, []).
--- a5/q2am.pl
+++ a5/q2am.pl
@@ -14,3 +14,44 @@ before( pm(H1,M1),pm(H2,M2)) :-
     H2 < H1 -> false;
     M1 < M2 -> true; false.
 
+
+next(am(_,_), pm(A,B)) :- false.
+next( pm(A,B),am(_,_)) :- false.
+
+next( pm(H1,M1),pm(H2,M2)) :-
+    M1 < M2 -> false;
+    M2 < M1 -> false;
+    H1 is (H2 - 1) -> true; false.
+
+next( am(H1,M1),am(H2,M1)) :-
+    H1 is (H2 - 1) -> true; false.
+
+next_hour(am(12,X),am(1,X)).
+next_hour(am(N,X),am(N1,X)) :- N>0, N<11, N1 is N+1.
+next_hour(am(11,X),pm(12,X)).
+next_hour(pm(12,X),pm(1,X)).
+next_hour(pm(N,X),pm(N1,X)) :- N>0, N<11, N1 is N+1.
+
+
+same([], []).
+
+same([H1|R1], [H2|R2]):-
+    H1 = H2,
+    same(R1, R2).
+
+del1(E, [], []) :- false.
+del1(E, E, []).
+del1(E, _, []) :- false.
+del1(E, [], _) :- false.
+del1(E , [H|T], [H2|T2]) :-
+    H = H2 -> del1(E, T, T2);
+    E = H -> same(T, T2); del1(E, T, T2).
+%    false.
+
+
+replace(_, _, [], []).
+replace(Old, New, [Old | L], [New | R]) :-
+    replace(Old, New, L, R).
+replace(Old, New, [Head | L], [Head | R]) :-
+    dif(Old,Head),
+    replace(Old, New, L, R).
