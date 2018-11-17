db8eeb6b51b777249f3c1cf6d978f40d2db96d6a
Carol Leung <carolleung8205@gmail.com>
2018-11-17 09:56:08 -0800

    refactored again to work without needing to add the types (columns of a table) to the KB

--- pokemon/pokemon.pl
+++ pokemon/pokemon.pl
@@ -1,41 +1,28 @@
 :- use_module(library(apply)).
 :- use_module(library(csv)).
 
-types("Normal", 1).
-types("Fire", 2).
-types("Water", 3).
-types("Electric", 4).
-types("Grass", 5).
-types("Ice", 6).
-types("Fighting", 7).
-types("Poison", 8).
-types("Ground", 9).
-types("Flying", 10).
-types("Psychic", 11).
-types("Bug", 12).
-types("Rock", 13).
-types("Ghost", 14).
-types("Dragon", 15).
-types("Dark", 16).
-types("Steel", 17).
-types("Fairy", 18).
-
 % taken from https://stackoverflow.com/questions/23183662/prolog-parsing-a-csv-file
 get_rows_data(File, Lists):-
   csv_read_file(File, Rows, []),
   maplist(row_to_list, Rows, Lists).
 
+% change default row(_,_,...) to a list so we could get value by index
 row_to_list(Row, List):-
   Row =.. [row|List].
 
 % general function to get cell data
-get_data(File, RowNo, ColumnNo, Value) :-
-  get_rows_data(File, AllData),
+get_data(AllData, RowNo, ColumnNo, Value) :-
   nth0(RowNo, AllData, DataForRow),
   nth0(ColumnNo, DataForRow, Value).
 
+% general function to get the name and number of a column
+get_col_number(AllData, ColName, ColNo) :-
+  nth0(0, AllData, AllColNames),
+  nth0(ColNo, AllColNames, ColName).
+
 % where D is defending pokemon, A is the Attacking pokemon
 get_attack_effectiveness(D, A, V):-
-  types(D, Dvalue),
-  types(A, Avalue),
-  get_data("types.csv", Dvalue, Avalue, V).
+  get_rows_data("types.csv", AllData),
+  get_col_number(AllData, D, Dvalue),
+  get_col_number(AllData, A, Avalue),
+  get_data(AllData, Dvalue, Avalue, V).
