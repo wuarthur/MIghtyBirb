2040b0b6f9eead066f2b292fdf328eb208cf7c3a
Carol Leung <carolleung8205@gmail.com>
2018-11-17 09:46:41 -0800

    refactored a bit, made general formula to get data from a cell

--- pokemon/pokemon.pl
+++ pokemon/pokemon.pl
@@ -23,19 +23,19 @@ types("Fairy", 18).
 % taken from https://stackoverflow.com/questions/23183662/prolog-parsing-a-csv-file
 get_rows_data(File, Lists):-
   csv_read_file(File, Rows, []),
-  rows_to_lists(Rows, Lists).
-
-rows_to_lists(Rows, Lists):-
   maplist(row_to_list, Rows, Lists).
 
 row_to_list(Row, List):-
   Row =.. [row|List].
 
+% general function to get cell data
+get_data(File, RowNo, ColumnNo, Value) :-
+  get_rows_data(File, AllData),
+  nth0(RowNo, AllData, DataForRow),
+  nth0(ColumnNo, DataForRow, Value).
 
 % where D is defending pokemon, A is the Attacking pokemon
-effectiveness_of_defense(D, A, V):-
-  get_rows_data("types.csv", TypeChart),
+get_attack_effectiveness(D, A, V):-
   types(D, Dvalue),
   types(A, Avalue),
-  nth0(Dvalue, TypeChart, Dstats),
-  nth0(Avalue, Dstats, V).
+  get_data("types.csv", Dvalue, Avalue, V).
