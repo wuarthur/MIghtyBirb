9ab9af861a8eb8ca30901127f184c78d6835fb72
Carol Leung <carolleung8205@gmail.com>
2018-11-17 16:03:27 -0800

    dual attack type weaknesses

--- pokemon/pokemon.pl
+++ pokemon/pokemon.pl
@@ -20,9 +20,23 @@ get_col_number(AllData, ColName, ColNo) :-
   nth0(0, AllData, AllColNames),
   nth0(ColNo, AllColNames, ColName).
 
-% where D is defending pokemon, A is the Attacking pokemon
+% where D is defending type, A is the Attacking type
+get_attack_effectiveness('',_,1).
 get_attack_effectiveness(D, A, V):-
   get_rows_data("types.csv", AllData),
   get_col_number(AllData, D, Dvalue),
   get_col_number(AllData, A, Avalue),
   get_data(AllData, Dvalue, Avalue, V).
+
+% where P is the defending Pokemon (of one or more types)
+% A is the attacking type
+% V is the multiplier of the damage
+get_pokemon_effectiveness(P, A, V):-
+  get_rows_data("pokedex.csv", AllData),
+  get_col_number(AllData, 'Type 1', T1No),
+  get_col_number(AllData, 'Type 2', T2No),
+  get_data(AllData, P, T1No, T1),
+  get_data(AllData, P, T2No, T2),
+  get_attack_effectiveness(T1, A, V1),
+  get_attack_effectiveness(T2, A, V2),
+  V is V1*V2.
