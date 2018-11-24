24300c632d2079a00e3a235deef798577eb6c27a
王淏 <wang.hao@alumni.ubc.ca>
2018-11-24 05:32:12 -0800

    added more notes

--- pokemon/game.pl
+++ pokemon/game.pl
@@ -4,3 +4,15 @@
 :- [battle].
 
 
+
+%%%%%%%%% Easier to update KB if we use same 'active_pokemons' for both.
+%%%%%%% We probably gonna use Idx for active pokemons, and update their health via DB as in battle.pl
+%%%%%%% less "INNTER LEFT JOIN" that way.
+
+
+active_pokemons('rival', []).
+active_pokemons('mine', []).
+
+
+
+%%%%%%%%% TODO next -- Pokemon team picking
